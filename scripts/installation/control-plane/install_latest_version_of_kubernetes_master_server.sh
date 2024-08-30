c0700c851c792b4e98f4b3335bb51e9142446876
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-08-30 19:54:17 +0200

    Update install_latest_version_of_kubernetes_master_server.sh
    Forgot to mention in the comments that the code snippet doesn`t just install Docker, but also changes the repository to override Debian default to use the official Docker repository.

--- scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
+++ scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
@@ -26,7 +26,7 @@ fi
 apt update
 apt install -y apt-transport-https ca-certificates curl
 
-# Install Docker
+# Add Docker repository and install Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
