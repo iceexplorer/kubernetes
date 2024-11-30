4e7db4cd87d2de0d0ab319304f48105bffeaac71
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-11-30 18:13:38 +0100

    Update install_latest_version_of_kubernetes_master_server.sh

--- scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
+++ scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
@@ -26,6 +26,9 @@ fi
 apt update
 apt install -y apt-transport-https ca-certificates curl
 
+# Disable swap
+swapoff -a
+
 # Add Docker repository and install Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -76,9 +79,6 @@ apt install -y kubeadm kubelet kubectl
 
 # Hold Kubernetes packages to prevent auto-updates
 apt-mark hold kubeadm kubelet kubectl
-
-# Disable swap
-swapoff -a
 
 # Set up hostname for the control plane
 hostnamectl set-hostname k8s-control-plane
