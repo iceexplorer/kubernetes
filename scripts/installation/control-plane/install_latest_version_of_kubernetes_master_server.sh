46c58f994cd5dc98e8eb50cfccfb7c3ad72eb7e1
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-11-30 19:54:29 +0100

    Update install_latest_version_of_kubernetes_master_server.sh
    upppsssss... installed Containerd before configuring it :D

--- scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
+++ scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
@@ -73,7 +73,7 @@ hostnamectl set-hostname "$MASTER_HOSTNAME"
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
-apt install -y docker-ce docker-ce-cli containerd.io
+apt install -y docker-ce docker-ce-cli
 
 # Start Docker service
 systemctl start docker
