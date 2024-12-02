7bd89e2c11ef4507ab17db9be0323bf09e12695c
IceExplorer <157909189+iceexplorer@users.noreply.github.com>
2024-12-02 08:47:03 +0100

    Update install_latest_version_of_kubernetes_master_server.sh
    Changed Kubernetes repo to version 1.31

--- scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
+++ scripts/installation/control-plane/install_latest_version_of_kubernetes_master_server.sh
@@ -108,8 +108,8 @@ containerd config default > /etc/containerd/config.toml
 systemctl restart containerd
 
 # Add Kubernetes apt repository
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
 
 # Update package list to get the latest versions
 apt update
